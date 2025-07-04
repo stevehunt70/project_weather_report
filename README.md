0c4b872dd1d958ee0e166fa958875e353db90877
stevehunt70 <steve@infineo-solutions.co.uk>
2025-07-04 16:08:38 +0100

    added in weather icons, change script to show new icons, complete readme.md, add in detail box for city

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-### change this bit show steve creating a branch
-# project_weather_report
+# project - 5 day weather report
+## create a 5 day weather report to show 5 days of your chosen city / town having the following features
+*. input field to type in your city
+*. button that gets the forecast from the weather website 'https://openweathermap.org'
+*. use the given API and code to retrieve the data from the relevant table
+*. Add any extra features that will enhance your weather report
+
+## create wireframe
+*. using MS Excel we created a layout for the webpage
+
+## web page
+*. input box to type in your chosen city / town
+*. button to confirm choice
+*. <div> strucure to pull in the extracted data for a 5 day forecast beginning with the current date
+*. collectively agreed to not use bootstrap for css styles and layout as it was relatively simple and easy to write the css
+*. use of google fonts to get font-family: "Nunito"
+
+## Javascript function in scripts.js
+*. Using the links provided we found the relevant data table for the extract
+*. create script to extract data for 5 day forecast
+
+## addition to the task
+*. using extra Javascript we extracted the 3 hour detail which allows the user to click a button for it to list the 3 hour detail. It was all in the same data array so why not use it
+*. use of different icons for the weather changing from the data extraction icons
+
+
+## links
+*. github repository: https://github.com/stevehunt70/project_weather_report.git
+*. github live view page: https://stevehunt70.github.io/project_weather_report/
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,19 @@ document.getElementById("getWeather").addEventListener("click", function () {
       if (geoData.length === 0)
         throw new Error("Location not found, try again. Be more specific!");
 
-      const { lat, lon, name, country } = geoData[0];
-      document.getElementById("titleForCity").textContent =
-        `Your 5-day forecast for ${name}, ${country}`;
+      const { lat, lon, name, country, state } = geoData[0];
+
+      //document.getElementById("titleForCity").textContent =
+      //  `Your 5-day forecast for ${name}, ${country}`;
+      document.getElementById('cityCard').innerHTML = `
+        <h4>Your 5-Day Forecast:</h4>
+        <p><strong>City: </strong>${name}</p>
+        <p><strong>Country: </strong>${country}, ${state}</p>
+        <p><strong>Longitude: </strong>${lon}</p>
+        <p><strong>Latitude: </strong>${lat}</p>
+        `;
+
+      document.querySelector('.detail-for-city').style.visibility = 'visible';
 
       const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
       return fetch(forecastUrl);
@@ -42,30 +52,35 @@ document.getElementById("getWeather").addEventListener("click", function () {
         const day = new Date(summary.dt_txt).toDateString();
         const temp = summary.main.temp;
         const desc = summary.weather[0].description;
-        const icon = summary.weather[0].icon;
+        //const icon = summary.weather[0].icon;
+        const customIcon = getCustomIcon(desc);
         const humidity = summary.main.humidity;
 
         document.getElementById(`WeatherResult${i}`).innerHTML = `
           <p><strong>Date: </strong>${day}</p>
           <p><strong>Temp: </strong>${temp} °C</p>
           <p><strong>Condition: </strong>${desc}</p>
+          <div><img src="assets/images/${customIcon}" alt="${desc}"></div>
           <p><strong>Humidity: </strong>${humidity}%</p>
-          <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${desc}">
-          <button onclick="showDetail(${i})" style="margin-left:10px;margin-bottom:10px">Show Details</button>
+          <div class="button-wrapper"><button onclick="showDetail(${i})" style="margin-left:10px;margin-bottom:10px">show 3hr detail</button></div>
         `;
-
+//<img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${desc}">
         document.querySelector('.weather-dashboard').style.visibility = 'visible';
 
         const hourlyHtml = `
           <h4>3 Hourly Forecast for ${day}</h4>
           <div style="display:flex; flex-wrap:wrap; gap:10px; margin-top:10px;">
             ${dayData.map(hour => {
+
+              const dateObj = new Date(hour.dt_txt);
               const time = new Date(hour.dt_txt).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-              const iconUrl = `https://openweathermap.org/img/wn/${hour.weather[0].icon}.png`;
+              const hourOfDay = dateObj.getHours()
+              const iconUrl = getCustomIcon(hour.weather[0].description, hourOfDay);
+              
               return `
                 <div class="weather-detail-box">
                   <strong>${time}</strong>
-                  <img src="${iconUrl}" alt="${hour.weather[0].description}" /> <br>
+                  <img src="assets/images/${iconUrl}" alt="${hour.weather[0].description}"/> <br>
                   <span>${hour.main.temp} °C</span> <br>
                   <small align="center">${hour.weather[0].description}</small>
                 </div>
@@ -93,3 +108,33 @@ function showDetail(dayIndex) {
     if (el) el.style.display = (i === dayIndex) ? "block" : "none";
   }
 }
+
+function getCustomIcon(desc, hour) {
+    const iconMap = {
+        "clear sky": "sunny.png",
+        "few clouds": "cloudy-sunny.png",
+        "scattered clouds": "scattered-clouds.png",
+        "broken clouds": "cloudy-sunny.png",
+        "shower rain": "cloudy-showers.png",
+        "light rain" : "cloudy-showers.png",
+        "rain": "rain.png",
+        "thunderstorm": "cloudy-storm-sunny.png",
+        "snow": "snow.png",
+        "mist": "mist.png",
+        "overcast clouds": "scattered-clouds.png",
+    }
+    const lowerDesc = desc.toLowerCase();
+    const key = Object.keys(iconMap).find(k => lowerDesc.includes(k));
+
+    if(lowerDesc === 'clear sky' && (hour < 6 || hour >= 20)) {
+      return "clear-night.png";
+    } else if((lowerDesc === 'scattered clouds' || lowerDesc ==='broken clouds' 
+      || lowerDesc ==='few clouds' || lowerDesc ==='overcast clouds')  
+      && (hour < 6 || hour >= 20)) {
+        return "cloudy-night.png"
+    } else if((lowerDesc === 'light rain' || lowerDesc === 'rain')
+      && (hour < 6 || hour >= 20)) {
+        return "rain-night.png"
+      }
+    return iconMap[key] || "sunny.png";
+};
